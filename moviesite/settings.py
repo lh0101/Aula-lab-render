64bc8033defe66df11e6d0c91df610dfec2af89e
Luiz Henrique Piffer Marques <luiz.piffermaques@usp.br>
2024-09-30 16:54:14 -0300

    Adaptar configurações do banco de dados para implantação no Render

--- moviesite/settings.py
+++ moviesite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -19,7 +19,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=600,
+    )
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-9$kvk_dsvd#5!b%y0py@e7_!9$o0=gktct*d!n=(g&z0r1^jzh"
 
