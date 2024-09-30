d5929ea7cbb3d6640f17d66f7b6a558df69f559b
Luiz Henrique Piffer Marques <luiz.piffermaques@usp.br>
2024-09-30 16:20:46 -0300

    Criar aplicação CRUD de filmes

--- movies/urls.py
+++ movies/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
-app_name = "movies"
+app_name = 'movies'
 urlpatterns = [
-    path("", views.list_movies, name="index"),
-    path("search/", views.search_movies, name="search"),
-    path("create/", views.create_movie, name="create"),
-    path("<int:movie_id>/", views.detail_movie, name="detail"),
+    path('', views.MovieListView.as_view(), name='index'),  # edite esta linha
+    path('search/', views.search_movies, name='search'),
+    path('create/', views.create_movie, name='create'),
+    path('<int:movie_id>/', views.detail_movie,
+         name='detail'),
+    path('update/<int:movie_id>/', views.update_movie, name='update'),
+    path('delete/<int:movie_id>/', views.delete_movie, name='delete'),
 ]
