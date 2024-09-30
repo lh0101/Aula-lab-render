d7b1fc28e3033337b32533ca124c724b0297e75b
Luiz Henrique Piffer Marques <luiz.piffermaques@usp.br>
2024-09-30 17:29:38 -0300

    Adicionar funcionalidades de resenha, listas e serviço de streaming

--- movies/admin.py
+++ movies/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Movie, Review
+from .models import Movie, Review, List, Provider
 
+admin.site.register(Provider) 
+admin.site.register(List)
 admin.site.register(Movie)
 admin.site.register(Review)
